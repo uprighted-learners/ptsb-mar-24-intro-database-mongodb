cd6fb65f24321eba4a24762c87ed5d51b7f70db2
Frank Chambergo <franky4908@gmail.com>
2024-05-15 19:30:47 -0400

    Unit 5 Review Day 2 Before Break

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true}));
 
 const port = 3000;
 
 const fruits = [
     {
-        id: 1,
+        id: uuidv4(),
         name: "banana"
     },
     {
-        id: 2,
+        id: uuidv4(),
         name: "mango"
     }
 ]
 
 // GET all route
 app.get("/fruits", (req, res) => {
+    console.log(fruits);
     res.json(fruits);
 });
 
@@ -29,12 +31,8 @@ app.get("/fruits/:id", (req, res) => {
     console.log(req.params);
     console.log(typeof req.params.id);
 
-    if (isNaN(req.params.id)) {
-        return res.status(406).send("Please insert a number for id")
-    }
-
     for (let fruit of fruits) {
-        if (fruit.id === parseInt(req.params.id)) {
+        if (fruit.id === req.params.id) {
             return res.status(200).send(fruit);
         }
     }
@@ -42,10 +40,46 @@ app.get("/fruits/:id", (req, res) => {
     res.status(404).send("Fruit not found.")
 })
 
-app.post("/fruits/add", (req, res) => {
+app.post("/fruits", (req, res) => {
     console.log(req.body);
 
-    res.send("POST Route Hit")
+    if (fruits.length > 0 && fruits.some((fruit) => fruit.name === req.body.name)) {
+        res.send("Fruit already exists");
+        return;
+    }
+
+    const newFruit = {
+        id: uuidv4(),
+        name: req.body.name
+    };
+
+    fruits.push(newFruit);
+    
+    res.send(newFruit);
+});
+
+app.put("/fruits/:id", (req, res) => {
+    for (let fruit of fruits) {
+        if (fruit.id === req.params.id) {
+            fruit.name = req.body.name;
+            res.status(200).send("Update Successful");
+            return;
+        }
+    }
+
+    res.status(404).send("Fruit not found");
+});
+
+app.delete("/fruits/:id", (req, res) => {
+    for (let i = 0; i < fruits.length; i++) {
+        if (fruits[i].id === req.params.id) {
+            fruits.splice(i, 1);
+            res.status(200).send("Fruit deleted");
+            return;
+        }
+    }
+
+    res.status(404).send("Fruit not found");
 })
 
 app.listen(port, () => {
